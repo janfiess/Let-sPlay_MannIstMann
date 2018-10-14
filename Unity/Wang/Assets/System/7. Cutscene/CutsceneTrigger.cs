e9977dedee6e1393f3cec517fcff82a6eca6b0c7
janfiess <fabi.images@gmail.com>
2018-10-14 14:13:34 +0200

    unity 3

---
+++ Unity/Wang/Assets/System/7. Cutscene/CutsceneTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.AI;
+
+
+public class CutsceneTrigger : MonoBehaviour {
+    public PlayableDirector cutsceneDirector;
+    public NavMeshAgent playerAgent;
+    public float agentPause = 10.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playerAgent.isStopped = true;
+        cutsceneDirector.Play();
+        StartCoroutine(PauseTimer());
+    }
+
+    private IEnumerator PauseTimer ()
+    {
+        yield return new WaitForSeconds(agentPause);
+        playerAgent.isStopped = false;
+    }
+
+}
