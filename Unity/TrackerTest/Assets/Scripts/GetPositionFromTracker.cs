fe355f50fc9eafc25193d67c7f352d2c0289d063
janfiess <fabi.images@gmail.com>
2018-10-14 13:49:55 +0200

    Unity 2

---
+++ Unity/TrackerTest/Assets/Scripts/GetPositionFromTracker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPositionFromTracker : MonoBehaviour {
+	public GameObject viveTracker;
+
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(viveTracker.transform.position.x + 0.3f, viveTracker.transform.position.y + 0.2f, viveTracker.transform.position.z);
+	}
+}
